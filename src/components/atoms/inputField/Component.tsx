b6728209f271c11af4e0c70524d3039f2c1a1e80
74win <ccw.la@outlook.com>
2023-03-21 15:42:13 +0100

    chore: change background color

--- src/components/atoms/inputField/Component.tsx
+++ src/components/atoms/inputField/Component.tsx
@@ -3,12 +3,13 @@ import TextField from '@mui/material/TextField';
 
 export const InputField = ():JSX.Element => {
     const style={
-        'background': '#F5C326',
-        '&:hover' : {
-            'background': '#EBB60C'
-        },
+        'background': '#F3F1F1',
+        // '&:hover' : {
+        //     'background': '#E2E2E2'
+        // },
         'width': 500,
-        'maxWidth': '100%'
+        'maxWidth': '100%',
+        'borderradius': 200,
 
     }
 
