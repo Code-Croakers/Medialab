6ccb8873d097aeaeb032c3b558527e854c623d9f
Annie Chen <a.janice.chen@gmail.com>
2023-03-21 16:20:27 +0100

    feat: add text bubble + styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,35 @@ import { TextBubble } from './components/atoms/textBubble/Component';
 function App() {
   return (
     <div className="App">
-        <Header/>
-        <main>
-          <SubmitBtn/>
-          <PrimaryBtn children='Start nieuwe sessie' />
-          <TextBubble children='bitch hello'/>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </main>
+      <Header />
+      <main>
+        <SubmitBtn />
+        <PrimaryBtn children='Start nieuwe sessie' />
+        <div>
+          <TextBubble children='bitch hello' sender='app' />
+          <TextBubble 
+            children="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+            officia deserunt mollit anim id est laborum." 
+            sender='user' />
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </main>
     </div>
   );
 }
--- src/components/atoms/textBubble/Component.tsx
+++ src/components/atoms/textBubble/Component.tsx
@@ -1,16 +1,22 @@
-import { Paper, Typography } from "@mui/material"
+import { Paper, Typography, TextField } from "@mui/material"
 
 interface Props{
     children: string
-    sender?: 'app' | 'user'
+    sender: 'app' | 'user'
 }
 
 export const TextBubble = (props: Props):JSX.Element => {
     const stylePaper = {
-        
+        'display': 'flex',
+        'justify-content': props.sender === 'app' ? 'flex-start' : 'flex-end',
+        'textAlign': 'left',
         'height': 'fit-content',
-        'padding': '24px',
-        'width': '50%'
+        'width': 'fit-content',
+        'maxWidth': '50%',
+        'padding': '16px',
+        'margin': '0',
+        'background': props.sender === 'app' ? '#F3F1F1' : '#D2EAFD',
+        'borderRadius' : props.sender === 'app' ? '24px 24px 24px 0' : '24px 24px 0 24px'
     }
 
     const styleP = {
