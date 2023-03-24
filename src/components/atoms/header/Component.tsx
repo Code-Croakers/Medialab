7f956804aec61cceca81e8858eab70682a8f1af6
Annie Chen <a.janice.chen@gmail.com>
2023-03-24 12:05:40 +0100

    chore: clean up code

--- src/components/atoms/header/Component.tsx
+++ src/components/atoms/header/Component.tsx
@@ -4,23 +4,28 @@ export const Header = ():JSX.Element => {
     const styleHeader={
         'background': '#F5C326',
         'height': '64px',
-        'display': "flex",
-        "justifyContent": "center",
-        "alignItems": "center",
-        "padding":"0 24px 0 24px",
-        "@media(min-width : 976px)":{
-            "alignItems": "flex-start",
+        'display': 'flex',
+        'justifyContent': 'center',
+        'alignItems': 'center',
+        'padding': '0 24px 0 24px',
+        '@media(min-width : 976px)':{
+            'alignItems': 'flex-start',
         }
     }
 
     const styleLogo={
-        "height": "42px",
-        "width":"72px"
+        'height': '42px',
+        'width':'72px'
     }
 
     return(
         <>
-        <AppBar position='static' sx={styleHeader}> <img src="/logoPrimary.png" style={styleLogo} alt="logo Virtuele Helden"/></AppBar>
+        <AppBar 
+            position='static' 
+            sx={styleHeader}
+            elevation={0}
+        > 
+            <img src='/logoPrimary.png' style={styleLogo} alt='logo Virtuele Helden'/></AppBar>
         </>
     )
 }
