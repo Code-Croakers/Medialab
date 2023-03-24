13103f4d85270767d76a04dbcee04ec25fb0da1c
74win <ccw.la@outlook.com>
2023-03-24 17:00:17 +0100

    feat: Added ConfirmationModal

--- src/components/atoms/primaryBtn/Component.tsx
+++ src/components/atoms/primaryBtn/Component.tsx
@@ -1,50 +1,32 @@
 import { Button, Fab } from '@mui/material'
-import AddIcon from '@mui/icons-material/Add';
-import { useEffect, useState } from 'react';
 
-interface Props{
+interface Props {
     children?: string
 }
 
-export const PrimaryBtn = (props: Props):JSX.Element => {
-    const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth)
-    
-    const style={
+export const PrimaryBtn = (props: Props): JSX.Element => {
+    const style = {
         'background': '#F5C326',
-        'color' : '#FFFFFF',
-        '&:hover' : {
+        'color': '#FFFFFF',
+        '&:hover': {
             'background': '#EBB60C'
         }
     }
 
     const startNewSession = (): void => {
         window.location.reload()
+        console.log('reload page')
     }
 
-    const windowResizeHandler = () => {
-        setWindowWidth(window.innerWidth)
-    }
-    useEffect(() => {
-      window.addEventListener("resize", windowResizeHandler, false);
-    }, []);
-
-    if(windowWidth <= 976){
-        return (
-            <Fab sx={style} aria-label='add' size='small'>
-                <AddIcon />
-            </Fab>
-        )
-    } else {
-        return(
-            <>
-                <Button 
-                sx={style} 
-                variant='contained' 
+    return (
+        <>
+            <Button
+                sx={style}
+                variant='contained'
                 onClick={() => startNewSession()}
-                >
-                    {props.children}
-                </Button>
-            </>
-        )
-    }
+            >
+                {props.children}
+            </Button>
+        </>
+    )
 }
