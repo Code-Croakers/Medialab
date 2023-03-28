5b6b01a1ff501b5d95ef97330468f391a647bd02
74win <ccw.la@outlook.com>
2023-03-28 17:49:16 +0200

    feat: add setTimeout for message

--- src/components/molecules/chatBox/Component.tsx
+++ src/components/molecules/chatBox/Component.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
 import { TextBubble } from '../../atoms/textBubble/Component'
 import styles from './style.module.css';
 
 export const ChatBox = (): JSX.Element => {
+    const ReturnSmth = ():JSX.Element =>{
+        return <TextBubble children='Wat heb je vandaag gedaan?' sender='app' />
+    }
+    const [active, setActive] = useState<boolean>(false)
+    useEffect(()=>{
+        const greeting = setTimeout(()=>{
+            setActive(true)
+        }, 2000)
+        return() => clearTimeout(greeting)
+    }, [])
+
     return (
         <div className={styles['chat-container']} id="chatBox">
             <TextBubble children='Hallo' sender='app' />
-            <TextBubble children='Wat heb je vandaag gedaan?' sender='app' />
+            { active ? <ReturnSmth/>: null }
         </div>
     )
 }
