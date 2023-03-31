841331bb7025fea3bbad40252075dc4b75547e86
Annie Chen <a.janice.chen@gmail.com>
2023-03-31 10:53:06 +0200

    feat: add timeout on all messages

--- src/components/molecules/chatBox/Component.tsx
+++ src/components/molecules/chatBox/Component.tsx
@@ -3,21 +3,35 @@ import { TextBubble } from '../../atoms/textBubble/Component'
 import styles from './style.module.css';
 
 export const ChatBox = (): JSX.Element => {
-    const ReturnSmth = ():JSX.Element =>{
+    const [greetingActive, setGreetingActive] = useState<boolean>(false)
+    const [secondMsgActive, setSecondMsgActive] = useState<boolean>(false)
+
+    const ReturnGreeting = ():JSX.Element =>{
+        return <TextBubble children='Hallo!' sender='app' />
+    }
+
+    const ReturnSecondMsg = ():JSX.Element =>{
         return <TextBubble children='Wat heb je vandaag gedaan?' sender='app' />
     }
-    const [active, setActive] = useState<boolean>(false)
+
     useEffect(()=>{
         const greeting = setTimeout(()=>{
-            setActive(true)
-        }, 2000)
+            setGreetingActive(true)
+        }, 1000)
         return() => clearTimeout(greeting)
+    }, [])
+
+    useEffect(()=>{
+        const secondMsg = setTimeout(()=>{
+            setSecondMsgActive(true)
+        }, 2000)
+        return() => clearTimeout(secondMsg)
     }, [])
 
     return (
         <div className={styles['chat-container']} id="chatBox">
-            <TextBubble children='Hallo' sender='app' />
-            { active ? <ReturnSmth/>: null }
+            { greetingActive ? <ReturnGreeting/> : null }
+            { secondMsgActive ? <ReturnSecondMsg/> : null }
         </div>
     )
 }
